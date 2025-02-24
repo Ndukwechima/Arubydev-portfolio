ffba8f2701e624cc6a3bdc76f2a03c0458c5bd36
chimae <chimandukwe.r@gmail.com>
2025-02-24 18:20:35 +0000

    Linked the projects for view

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -98,6 +98,7 @@ const ProjectsMain = () => {
               year={project.year}
               align={project.align}
               image={project.image}
+              link={project.link}
             />
           );
         })}
