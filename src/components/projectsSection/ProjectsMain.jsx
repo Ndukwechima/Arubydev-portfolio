f186d1fb1a2b56e3189b34fb2276b3a892456b69
chimae <chimandukwe.r@gmail.com>
2025-06-08 09:04:48 +0000

    Added more projects and discard some old ones

--- src/components/projectsSection/ProjectsMain.jsx
+++ src/components/projectsSection/ProjectsMain.jsx
@@ -4,21 +4,27 @@ import { motion } from "framer-motion";
 import { fadeIn } from "../../framerMotion/variants";
 import project1 from '../../assets/images/p6.png'
 import project2 from "../../assets/images/p8.png";
-import project3 from "../../assets/images/p13.png";
 import project4 from "../../assets/images/p10.png";
 import project5 from "../../assets/images/p7.png";
 import project6 from "../../assets/images/p11.png";
-import project7 from "../../assets/images/p3.png";
-import project8 from "../../assets/images/p9.png";
+import project9 from "../../assets/images/p9.png";
+import project14 from "../../assets/images/p14.png";
+import project15 from "../../assets/images/p15.png";
+import project16 from "../../assets/images/p16.png";
+import project17 from "../../assets/images/p17.png";
+
+
+
 
 
 const projects = [
-  {
-    name: "E-commerce App",
-    year: "May 2024",
+
+ {
+    name: "Next.js Burger Landing page",
+    year: "Nov 2023",
     align: "right",
-    image: project1,
-    link: "https://e-shop-next-typescript.vercel.app/",
+    image: project9,
+    link: "https://burger-website-kappa.vercel.app",
   },
 
   {
@@ -32,50 +38,68 @@ const projects = [
   {
     name: "Modern Dashboard UI",
     year: "Sept 2024",
-    align: "left",
+    align: "right",
     image: project2,
     link: "https://modern-react-admin-dashboard-ui.vercel.app/",
   },
 
   {
+    name: "React Comfy-store",
+    year: "Jan 2025",
+    align: "left",
+    image: project14,
+    link: "https://comfy-store-peach-one.vercel.app/",
+  },
+
+  {
+    name: "React Job Tracking App",
+    year: "April 2025",
+    align: "right",
+    image: project15,
+    link: "https://jobstify-ndukwe-chimas-projects.vercel.app/",
+  },
+
+  {
+    name: "React Github User Finder",
+    year: "May 2025",
+    align: "left",
+    image: project16,
+    link: "https://github-user-finder-self.vercel.app/",
+  },
+
+  {
+    name: "Next.js Cote Royale",
+    year: "May June",
+    align: "right",
+    image: project17,
+    link: "https://cote-royale-khaki.vercel.app/",
+  },
+
+  {
     name: "Movie Web App",
     year: "Dec 2023",
-    align: "right",
+    align: "left",
     image: project5,
     link: "https://movie-web-app-iota.vercel.app/",
   },
 
   {
-    name: "Agro Business Landing page",
-    year: "Dec 2024",
-    align: "left",
-    image: project8,
-    link: "https://okenkwu-theta.vercel.app/",
+    name: "Next.j E-commerce App",
+    year: "May 2024",
+    align: "right",
+    image: project1,
+    link: "https://e-shop-next-typescript.vercel.app/",
   },
 
-  {
-    name: "Doctors Consulting UI",
-    year: "Jan 2025",
-    align: "right",
-    image: project3,
-    link: "https://med-consult.vercel.app/",
-  },
 
   {
     name: "Restaurant Landing Page",
     year: "Jan 2025",
-    align: "left",
+    align: "right",
     image: project6,
     link: "https://restaurant-single-page-website-javascript.vercel.app/",
   },
 
-  {
-    name: "Modern Multi-Pages Website",
-    year: "Jan 2025",
-    align: "right",
-    image: project7,
-    link: "https://modern-website-javascript.netlify.app/",
-  },
 ];
 
 const ProjectsMain = () => {
